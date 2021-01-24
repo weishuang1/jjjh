e5b9e87a22f42870b18cae1f43e6ed0e895483dd
ws <15071837718@163.com>
2021-01-24 19:38:36 +0800

    34

---
+++ test_data/test_data.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+# @Time : 2021/1/22 21:22 
+# @Author : Lemon_Tricy
+# @QQ: 2378807189
+# Copyright：湖南省零檬信息技术有限公司
+
+data = {"url":"http://erp.lemfix.com",
+        "username":"test123",
+        "password":"123456",
+        "key":"841"
+        }
